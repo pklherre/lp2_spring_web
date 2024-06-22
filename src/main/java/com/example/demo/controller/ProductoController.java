1dfb71b119454efd5c709927ac4d9e3b5da66da4
Klevert Ayrton <vertkle@gmail.com>
2024-06-22 18:38:04 -0500

    logout implementado

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.UsuarioEntity;
 import com.example.demo.service.UsuarioService;
 
 @Controller
@@ -20,6 +21,11 @@ public class ProductoController {
 		if(session.getAttribute("usuario") == null) {
 			return "redirect:/";
 		}
+		
+		String correo = session.getAttribute("usuario").toString();
+		UsuarioEntity usuarioEntity = usuarioService.buscarUsuarioPorCorreo(correo);
+		model.addAttribute("foto", usuarioEntity.getUrlImagen());
+		
 		
 		return "menu";
 	}
