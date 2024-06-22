622e32e02c499efad0b9d35e06d682c215f5516b
Klevert Ayrton <vertkle@gmail.com>
2024-06-22 18:44:28 -0500

    lista productos

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.ProductoEntity;
 import com.example.demo.entity.UsuarioEntity;
+import com.example.demo.service.ProductoService;
 import com.example.demo.service.UsuarioService;
 
 @Controller
@@ -15,6 +19,8 @@ public class ProductoController {
 	
 	@Autowired
 	private UsuarioService usuarioService;
+	@Autowired
+	private ProductoService productoService;
 	
 	@GetMapping("/menu")
 	public String showMenu(HttpSession session, Model model) {
@@ -26,6 +32,8 @@ public class ProductoController {
 		UsuarioEntity usuarioEntity = usuarioService.buscarUsuarioPorCorreo(correo);
 		model.addAttribute("foto", usuarioEntity.getUrlImagen());
 		
+		List<ProductoEntity>productos = productoService.buscarTodosProductos();
+		model.addAttribute("productos", productos);
 		
 		return "menu";
 	}
